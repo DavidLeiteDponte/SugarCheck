47d17cdd881ef3e1295ad5d5e53c3a2b81492f10
Jose Leite <josegabrielleitedeponte@gmail.com>
2026-05-28 17:36:12 -0400

    hola

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,7 @@ import Home from "./features/home";
 import Login from "./features/login";
 import RecoveryPassword from "./features/recoveryPassword";
 import Register from "./features/register";
+import Dashboard from "./features/dashboard";
 
 
 function App() {
@@ -15,6 +16,8 @@ function App() {
           <Route path='/login' element={<Login />} />
           <Route path='/recoveryPassword' element={<RecoveryPassword />} />
           <Route path='/register' element={<Register />} />
+          <Route path='/dashboard' element={<Dashboard />} />
+
         </Routes>
       </Router>
     </ThemeWrapperContext>
---
+++ frontend/src/features/dashboard.tsx
@@ -0,0 +1,13 @@
+import Navbar from '../components/layout/Header/Navbar.tsx';
+import { Box, Typography, Link } from '@mui/material';
+import { useTheme } from '@mui/material/styles';
+
+
+export default function Dashboard() {
+
+  return (<>
+    <Navbar />
+    <h1>Hola Guerrero David</h1>
+  </>
+  );
+}

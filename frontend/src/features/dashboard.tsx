1585c44445001a9e1ab4d4336358c55edc31c345
Jose Leite <josegabrielleitedeponte@gmail.com>
2026-06-07 22:58:56 -0400

    hola

--- frontend/src/features/dashboard.tsx
+++ frontend/src/features/dashboard.tsx
@@ -1,6 +1,5 @@
 import Navbar from '../components/layout/Header/Navbar.tsx';
-import { Box, Typography, Link } from '@mui/material';
-import { useTheme } from '@mui/material/styles';
+
 
 
 export default function Dashboard() {
